9c5afed040bfcd196d5eb8d9501a049dc0187c4a
timjan <tjadeglans@gmail.com>
2025-02-06 00:17:54 +0100

    first working version

--- Extension.md
+++ Extension.md
@@ -1,5 +1,29 @@
-![Icon](https://raw.githubusercontent.com/GithubUser/OneWare.PslExtension/main/Icon.png)
+![image](https://raw.githubusercontent.com/timonix/OneWare.PslExtension/main/Icon.png)
 
 ### Get Started
 
-> This text will be visible in the builtin package manager
+# PSL Extension
+
+## Overview
+This extension integrates **PSL** as a simulator with OneWare Studio, enabling direct interaction with PSL simulations without additional configuration overhead.
+Should support Windows, Linux and MacOS. But only tested on Windows.
+
+## Requirements
+To use this extension, ensure you have a container runtime installed and properly configured. Currently tested with **Docker Desktop** and **Rancher Desktop**.
+
+## Installation
+- Install extension
+- Install a container runtime
+
+## Usage
+- Launch your container runtime, if not already running
+- PSL simulator now shows up in simulator list, simulate will now create a .sby file with some default settings and try to run it.
+- Docker image is downloaded automatically on first run, may take a while (about 1 GB)
+
+## Notes
+- The generated sby files are a starting point. You may have to edit paths or settings. 
+- sby files from other projects likely works with minor changes.
+
+## Why Use This Extension?
+- Streamlines **PSL** simulation execution.
+
--- src/OneWare.PslExtension/Services/PslService.cs
+++ src/OneWare.PslExtension/Services/PslService.cs
@@ -1,4 +1,7 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using System.Text;
+using System.Text.RegularExpressions;
+using AvaloniaEdit.Utils;
+using CommunityToolkit.Mvvm.Input;
 using DynamicData.Binding;
 using OneWare.Essentials.Enums;
 using OneWare.Essentials.Models;
@@ -20,9 +23,7 @@ public class PslService
     private readonly IOutputService _outputService;
     private readonly ISettingsService _settingsService;
     private readonly IProjectExplorerService _projectExplorerService;
-
-    private string _path = string.Empty;
-
+    
     public PslService(ILogger logger, IDockService dockService, ISettingsService settingsService, IPackageService packageService, IChildProcessService childProcessService, IEnvironmentService environmentService,
         IOutputService outputService, IProjectExplorerService projectExplorerService)
     {
@@ -37,18 +38,7 @@ public class PslService
         
     }
     
-
-    public async Task<bool> GenerateSbyAsync(IProjectFile file)
-    {
-        return true;
-    }
-    
-    public async Task<bool> GenerateAndSimulateSbyAsync(IProjectFile file)
-    {
-        return true;
-    }
-    
-    static string GetRelativePath(string fromPath, string toPath)
+    private static string GetRelativePath(string fromPath, string toPath)
     {
         Uri fromUri = new Uri(fromPath.EndsWith("\\") ? fromPath : fromPath + "\\");
         Uri toUri = new Uri(toPath);
@@ -56,16 +46,169 @@ public class PslService
         Uri relativeUri = fromUri.MakeRelativeUri(toUri);
         string relativePath = Uri.UnescapeDataString(relativeUri.ToString());
         return relativePath;
-        return relativePath.Replace('/', '\\'); // Convert to Windows-style path
+    }
+    
+    static string GetRelativeDescription(string filePath)
+    {
+        int depth = filePath.Count(c => c == Path.DirectorySeparatorChar);
+        return string.Concat(Enumerable.Repeat("..\\", depth));
+    }
+    
+    static HashSet<string> ExtractComponents(string filePath)
+    {
+        HashSet<string> components = new HashSet<string>();
+        string pattern = @"\s*component\s+(\w+)";
+
+        try
+        {
+            foreach (string line in File.ReadLines(filePath))
+            {
+                Match match = Regex.Match(line, pattern);
+                if (match.Success)
+                {
+                    components.Add(match.Groups[1].Value);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error reading file: {ex.Message}");
+        }
+
+        return components;
+    }
+    static HashSet<string> ExtractEntities(string filePath)
+    {
+        HashSet<string> entities = new HashSet<string>();
+        string pattern = @"\s+work\.(\w+)";
+
+        try
+        {
+            foreach (string line in File.ReadLines(filePath))
+            {
+                Match match = Regex.Match(line, pattern);
+                if (match.Success)
+                {
+                    entities.Add(match.Groups[1].Value);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error reading file: {ex.Message}");
+        }
+
+        return entities;
+    }
+    
+    public async Task<bool> GenerateSbyAsync(IProjectFile file)
+    {   
+        var context = await TestBenchContextManager.LoadContextAsync(file);
+        var engine = context.GetBenchProperty(nameof(PslSimulatorToolbarViewModel.BmcEngine)) ?? "smtbmc";
+        var mode = context.GetBenchProperty(nameof(PslSimulatorToolbarViewModel.BmcMode)) ?? "bmc";
+        var solver = context.GetBenchProperty(nameof(PslSimulatorToolbarViewModel.Solver)) ?? "z3";
+        var depth = context.GetBenchProperty(nameof(PslSimulatorToolbarViewModel.SearchDepth)) ?? "25";
+        var entity = Path.GetFileNameWithoutExtension(file.Name);
+        
+        if(file.Root is not UniversalFpgaProjectRoot root) return false;
+        
+        HashSet<IProjectFile> usedFiles = new HashSet<IProjectFile>();
+        usedFiles.Add(file);
+        
+        for (int i = 0; i < 10; i++)
+        {
+            foreach (var f in usedFiles)
+            {
+                Console.WriteLine(f.Name);
+            }
+            Console.WriteLine($"----{i}-----");
+            
+            HashSet<string> candidates = new HashSet<string>();
+            HashSet<IProjectFile> next = new HashSet<IProjectFile>();
+            foreach (var f in usedFiles)
+            {
+                
+                candidates.AddRange(ExtractEntities(f.FullPath));
+                candidates.AddRange(ExtractComponents(f.FullPath));
+        
+                next.AddRange(root.Files
+                    .Where(x => !root.CompileExcluded.Contains(x))
+                    .Where(x => candidates.Contains(Path.GetFileNameWithoutExtension(x.Name))));
+
+            }
+            usedFiles.AddRange(next);
+        }
+        
+        var relativePath = GetRelativeDescription(file.RelativePath);
+        var vhdlFilesWithRelativePaths = usedFiles
+            .Where(x => !root.CompileExcluded.Contains(x))
+            .Where(x => x.Extension is ".vhd" or ".vhdl")
+            .Select(x => $"{relativePath}{x.RelativePath}");
+        
+        var vhdlFilesNamesOnly = usedFiles
+            .Where(x => !root.CompileExcluded.Contains(x))
+            .Where(x => x.Extension is ".vhd" or ".vhdl")
+            .Select(x => x.Name);
+        
+        var sb = new StringBuilder();
+    
+        sb.AppendLine("[tasks]");
+        sb.AppendLine("bmc");
+        sb.AppendLine();
+    
+        sb.AppendLine("[options]");
+        sb.AppendLine($"depth {depth}");
+        sb.AppendLine($"bmc: mode {mode}");
+        sb.AppendLine();
+    
+        sb.AppendLine("[engines]");
+        sb.AppendLine($"bmc: {engine} {solver}");
+        sb.AppendLine();
+    
+        sb.AppendLine("[script]");
+        sb.Append("bmc: ghdl --std=08 ");
+        sb.Append(string.Join(" ", vhdlFilesNamesOnly.Select(x => x)));
+        sb.AppendLine($" -e {entity}");
+        sb.AppendLine($"prep -top {entity}");
+        sb.AppendLine();
+    
+        sb.AppendLine("[files]");
+        foreach (var vhdlFile in vhdlFilesWithRelativePaths)
+        {
+            sb.AppendLine(vhdlFile.Replace("\\","/"));
+        }
+        
+        var outputFilePath = $"{file.TopFolder.FullPath}/{entity}.sby";
+
+        try
+        {
+            await File.WriteAllTextAsync(outputFilePath, sb.ToString());
+            Console.WriteLine($"SBY file successfully generated at {outputFilePath}");
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error writing SBY file: {ex.Message}");
+            return false;
+        }
+        
+        return true;
+    }
+    
+    public async Task<bool> GenerateAndSimulateSbyAsync(IProjectFile file)
+    {
+        await GenerateSbyAsync(file);
+
+        return true;
     }
 
     public async Task<bool> RunSbyAsync(IProjectFile file)
     {
 
         var userPath = file.Root.FullPath;
-        var containerWorkspace = "/workspace";
-        var containerImage = "hdlc/formal";
-        var yosysCommand = "yosys -m ghdl";
+        const string containerWorkspace = "/workspace";
+        const string containerImage = "hdlc/formal";
+        const string yosysCommand = "yosys -m ghdl";
         var sbyFileName = file.Name;
         
         var relativePath = GetRelativePath(file.Root.FullPath, file.TopFolder.FullPath);
